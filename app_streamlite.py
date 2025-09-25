da60df9ab4bb853a55fa935da86638b795c86e0a
Stefanivi <61399733+Stefanivi@users.noreply.github.com>
2025-09-24 18:50:18 -0600

    app 1.1

--- app_streamlite.py
+++ app_streamlite.py
@@ -8,19 +8,3 @@ import streamlit as st
 #Definición de los parámetros de la aplicación
 file = st.sidebar.file_uploader("Sube tu Excel (.xlsx)", type=["xlsx"])
 sheet = st.sidebar.text_input("Nombre de la hoja", value="resultado")
-
-st.sidebar.markdown("### Columna de tiempo")
-time_mode = st.sidebar.radio("¿Cómo viene la fecha/tiempo?",
-                             ["date (timestamp)", "year + quarter"], index=1)
-
-date_col = None
-year_col = None
-quarter_col = None
-
-if time_mode == "date (timestamp)":
-    date_col = st.sidebar.text_input("Nombre de la columna de fecha", value="date")
-else:
-    year_col = st.sidebar.text_input("Columna de año", value="year")
-    quarter_col = st.sidebar.text_input("Columna de trimestre", value="quarter")
-
-btn = st.sidebar.button("Cargar y listar variables")
