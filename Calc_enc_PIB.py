03e1cc0248072e356b8a571832d3e77dd9e03d54
paulogarrido16-lab <paulogarrido16@gmail.com>
2025-09-24 18:50:34 -0600

    fase_rev_carga_excel

---
+++ Calc_enc_PIB.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+# Leer archivo Excel
+df = pd.read_excel("PIB_original.xlsx", sheet_name="series", engine="openpyxl")
+
+print("Lectura OK")
+df.info
+
+#___________________________________
+#Fase de revisión de la data contenido en el archivo de entrada
+#___________________________________
+
+#valores nulos
+print(df.isnull().sum())
+
+# Revisar si cada año tiene 4 trimestres
+conteo_trimestres = df.groupby("year")["quarter"].nunique()
+print(conteo_trimestres)
